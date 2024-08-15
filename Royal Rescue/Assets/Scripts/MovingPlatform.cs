2f4c6c4b52993aa4dcebc581d1c05581db846a9f
Jang-wj <jwj5230@gmail.com>
2024-08-15 21:48:51 +0900

    fix : 움직이면서 돌아가는 기능 구현
    MovingPlatform에서 SpinningPlatform 가져와서 사용

--- Royal Rescue/Assets/Scripts/MovingPlatform.cs
+++ Royal Rescue/Assets/Scripts/MovingPlatform.cs
@@ -9,15 +9,16 @@ public class MovingPlatform : MonoBehaviour
     [SerializeField] float speedY = 1f;
     [SerializeField] float speedZ = 1f;
     [SerializeField] float area = 5f;
-
+    [SerializeField] SpinningPlatform spinningPlatform;
     Rigidbody rb;
     Vector3 startPos;
+    bool bspin = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
         startPos = transform.position;
+        bspin = spinningPlatform != null;
     }
 
     void FixedUpdate()
@@ -25,9 +26,14 @@ public class MovingPlatform : MonoBehaviour
         if ((transform.position - startPos).magnitude >= area)
         {
             speedX *= -1;
+
+            if (bspin)
+            {
+                spinningPlatform.ChangeDirection();
+            }
         }
 
         Vector3 speed = new(speedX, speedY, speedZ);
-        rb.MovePosition(transform.position + speed * Time.deltaTime);
+        rb.MovePosition(transform.position + speed * Time.fixedDeltaTime);
     }
 }
--- Royal Rescue/Assets/Scripts/SpinningPlatform.cs
+++ Royal Rescue/Assets/Scripts/SpinningPlatform.cs
@@ -1,26 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SpinningPlatform : MonoBehaviour
 {
     [SerializeField] float speedX = 1f;
     [SerializeField] float speedY = 1f;
     [SerializeField] float speedZ = 1f;
-    Rigidbody rb;
+    [SerializeField] bool isLoop = false;
+    [SerializeField] float loopPoint = 30f;
+    private Rigidbody rb;
     Vector3 EulerAngleVelocity;
 
-    // Start is called before the first frame update
+    public float delta = 1.5f;  // Amount to move left and right from the start point
+    public float speed = 2.0f;
+    public float direction = 1;
+    private Quaternion startPos;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         EulerAngleVelocity = new Vector3(speedX, speedY, speedZ);
+        
+        startPos = transform.rotation;
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         Quaternion deltaRotation = Quaternion.Euler(EulerAngleVelocity * Time.fixedDeltaTime);
         rb.MoveRotation(rb.rotation * deltaRotation);
+
+        if (isLoop)
+        {
+            Quaternion a = startPos;
+
+            a.z += direction * (delta * Mathf.Sin(Time.time * speed));
+            transform.rotation = a;
+        }
     }
+    public void ChangeDirection()
+    {
+        speedX *= -1;
+        speedY *= -1;
+        speedZ *= -1;
+        EulerAngleVelocity = new Vector3(speedX, speedY, speedZ);
+    }
+
 }
