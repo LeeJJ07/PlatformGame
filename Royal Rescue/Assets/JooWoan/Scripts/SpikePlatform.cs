eb516756cedb4280ea2480d9f23c56f2b14fc4d0
catnip144 <skytower144@naver.com>
2024-08-15 20:51:16 +0900

    fix: 가시 플랫폼 효과 작용 방식 변경

--- Royal Rescue/Assets/JooWoan/Scripts/SpikePlatform.cs
+++ Royal Rescue/Assets/JooWoan/Scripts/SpikePlatform.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class SpikePlatform : MonoBehaviour
 {
-    [SerializeField] private ParticleSystem hitEffect;
+    [SerializeField] private GameObject hitEffect;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("플레이어의 체력을 ?만큼 감소.");
 
-            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+            PlayerController player = GameDirector.instance.PlayerControl;
+
+            // rigidbody 도 public으로 빼서 접근할 수 있으면 좋을 것 같습니다
             Rigidbody playerRb = player.GetComponent<Rigidbody>();
 
             playerRb.velocity = new Vector3(0, 0, 0);
             playerRb.AddForce(Vector3.left * 15f, ForceMode.Impulse);
             playerRb.AddForce(Vector3.up * 10f, ForceMode.Impulse);
 
-            hitEffect.Play();
+            GameObject effect = Instantiate(hitEffect, transform.parent);
+            effect.GetComponent<ParticleSystem>().Play();
+            Destroy(effect, 0.4f);
         }
     }
 }
