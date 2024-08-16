b8ac3d734babf14d2fe2f9c2302af5ec37def9fa
Jang-wj <jwj5230@gmail.com>
2024-08-16 15:07:35 +0900

    feat : SpinningPlatform 좌우 반복해서 회전 구현
    변수 이름 변경

--- Royal Rescue/Assets/Scripts/SpinningPlatform.cs
+++ Royal Rescue/Assets/Scripts/SpinningPlatform.cs
@@ -5,46 +5,44 @@ using UnityEngine.UIElements;
 
 public class SpinningPlatform : MonoBehaviour
 {
-    [SerializeField] float speedX = 1f;
-    [SerializeField] float speedY = 1f;
-    [SerializeField] float speedZ = 1f;
-    [SerializeField] bool isLoop = false;
-    [SerializeField] float loopPoint = 30f;
+    [SerializeField] float angleX = 1f;
+    [SerializeField] float angleY = 1f;
+    [SerializeField] float angleZ = 1f;
+    [SerializeField] bool isLoop = true;
+    [SerializeField] float loopSpeed = 2.0f;
+
     private Rigidbody rb;
     Vector3 EulerAngleVelocity;
-
-    public float delta = 1.5f;  // Amount to move left and right from the start point
-    public float speed = 2.0f;
-    public float direction = 1;
     private Quaternion startPos;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        EulerAngleVelocity = new Vector3(speedX, speedY, speedZ);
+        EulerAngleVelocity = new Vector3(angleX, angleY, angleZ);
         
         startPos = transform.rotation;
     }
 
     void FixedUpdate()
     {
-        Quaternion deltaRotation = Quaternion.Euler(EulerAngleVelocity * Time.fixedDeltaTime);
-        rb.MoveRotation(rb.rotation * deltaRotation);
-
         if (isLoop)
         {
             Quaternion a = startPos;
-
-            a.z += direction * (delta * Mathf.Sin(Time.time * speed));
-            transform.rotation = a;
+            Quaternion deltaRotation = Quaternion.Euler(EulerAngleVelocity * Mathf.Sin(Time.time * loopSpeed));
+            rb.MoveRotation(a * deltaRotation);
+        }
+        else
+        {
+            Quaternion deltaRotation = Quaternion.Euler(EulerAngleVelocity * Time.fixedDeltaTime);
+            rb.MoveRotation(rb.rotation * deltaRotation);
         }
     }
     public void ChangeDirection()
     {
-        speedX *= -1;
-        speedY *= -1;
-        speedZ *= -1;
-        EulerAngleVelocity = new Vector3(speedX, speedY, speedZ);
+        angleX *= -1;
+        angleY *= -1;
+        angleZ *= -1;
+        EulerAngleVelocity = new Vector3(angleX, angleY, angleZ);
     }
 
 }
