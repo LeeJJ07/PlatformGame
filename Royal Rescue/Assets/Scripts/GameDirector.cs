f1af1f07c565c3797c79d54d02d6537002f3b272
skytower144 <127186795+catnip144@users.noreply.github.com>
2024-08-17 22:55:26 +0900

    merge: 플레이어 오브젝트를 Room에 연동
    * 호성님께서 만드신 카메라 스크립트를 제가 다루는게 서툴러서 일단 제가 만든 카메라 스크립트를 적용시켰습니다.
    
    * 에셋 및 폴더 정리

---
+++ Royal Rescue/Assets/Scripts/GameDirector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour
+{
+    private static GameDirector _instance = null;
+    public static GameDirector instance => _instance;
+
+    public PlayerControlManagerFix PlayerControl;
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+}
