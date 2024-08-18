f74fb6e1ba68661c5c2d35578e540a6f084ff4c6
LeeJJ07 <jkj9513@naver.com>
2024-08-18 17:52:01 +0900

    style : 코드정리

--- Royal Rescue/Assets/normalmonster/Codes/Monster.cs
+++ Royal Rescue/Assets/normalmonster/Codes/Monster.cs
@@ -11,18 +11,15 @@ public class Monster : MonoBehaviour
     [SerializeField] private ChaseState chaseState;
     [SerializeField] private AttackState attackState;
     [SerializeField] private DeathState deathState;
-    //[SerializeField] private HurtState hurtState;
-
 
     private MonsterStateContext monsterStateContext;
     private EState curState;
 
     public GameObject player;
     private Animator animator;
-    private Collider collider;
+    private Collider coll;
 
-
-    public bool isDetect { get; set; }
+    public bool isDetect;
     [SerializeField] 
     private float maxHp = 100f;
     private float curHp = 100f;
@@ -48,7 +45,8 @@ public class Monster : MonoBehaviour
     private void Start()
     {
         if (!animator) animator = GetComponent<Animator>();
-        if(!collider) collider = GetComponent<Collider>();
+        if(!coll) coll = GetComponent<Collider>();
+        
         monsterStateContext = new MonsterStateContext(this);
         monsterStateContext.Transition(patrolState);
         curState = EState.PATROL;
@@ -65,32 +63,34 @@ public class Monster : MonoBehaviour
     private void Update()
     {
         if (Die())
-            UpdateState(EState.DEATH);
-        else
         {
-            switch (curState)
-            {
-                case EState.PATROL:
-                    if (CanSeePlayer())
-                    {
-                        isDetect = true;
-                        UpdateState(EState.CHASE);
-                    }
-                    break;
-                case EState.CHASE:
-                    if (CantChase() && !CanSeePlayer())
-                        UpdateState(EState.PATROL);
-                    if (CanAttackPlayer())
-                        UpdateState(EState.ATTACK);
-                    break;
-                case EState.ATTACK:
-                    if (!CanAttackPlayer())
-                    {
-                        isDetect = false;
-                        UpdateState(EState.CHASE);
-                    }
-                    break;
-            }
+            UpdateState(EState.DEATH);
+            monsterStateContext.CurrentState.UpdateState();
+            return;
+        }
+
+        switch (curState)
+        {
+            case EState.PATROL:
+                if (CanSeePlayer())
+                {
+                    isDetect = true;
+                    UpdateState(EState.CHASE);
+                }
+                break;
+            case EState.CHASE:
+                if (CantChase() && !CanSeePlayer())
+                    UpdateState(EState.PATROL);
+                if (CanAttackPlayer())
+                    UpdateState(EState.ATTACK);
+                break;
+            case EState.ATTACK:
+                if (!CanAttackPlayer())
+                {
+                    isDetect = false;
+                    UpdateState(EState.CHASE);
+                }
+                break;
         }
         monsterStateContext.CurrentState.UpdateState();
     }
@@ -117,15 +117,14 @@ public class Monster : MonoBehaviour
                 break;
         }
     }
+    #region 필요한 setter, getter
     public float getSpeed() { return speed; }
     public void setSpeed(float speed) { this.speed = speed; }
     public float getDamage() { return damage; }
     public float getFacingDir() { return facingDir; }
-    public void setFacingDir(float dir) { this.facingDir = dir / Mathf.Abs(dir); }
-    public float getDistanceOther(GameObject other)
-    {
-        return Mathf.Abs((other.transform.position - transform.position).magnitude);
-    }
+    #endregion
+
+    #region 전이조건
     bool CanSeePlayer()
     {
         //player가 시야각 안에 있는가
@@ -159,7 +158,54 @@ public class Monster : MonoBehaviour
     {
         return curHp <= 0;
     }
-    public Vector3 AngleToDir(float angle)
+    #endregion
+
+    #region 피격
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!animator.GetBool("isLive"))
+            return;
+
+        if (other == null)
+            return;
+
+        if (other.gameObject.tag == "PlayerAttack")
+            StartCoroutine(OnDamage());
+        
+    }
+    IEnumerator OnDamage()
+    {
+        animator.SetTrigger("takeAttack");
+        curHp -= player.GetComponent<PlayerController>().damage;
+
+        coll.enabled = false;
+        yield return new WaitForSeconds(1f);
+        coll.enabled = true;
+        Debug.Log("공격 받았음");
+    }
+    #endregion
+
+    #region 지나칠 때
+    private void OnTriggerStay(Collider other)
+    {
+        if (!animator.GetBool("isLive"))
+            return;
+        if (other == null)
+            return;
+        if (other.gameObject.tag == "Player")
+        {
+            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+            rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
+
+            Vector3 dir = new Vector3(other.gameObject.transform.position.x - transform.position.x, 0f, 0f).normalized;
+            rb.AddForce(dir * 15f, ForceMode.Impulse);
+            Debug.Log("밀어내기");
+        }
+    }
+    #endregion
+
+    #region 공용함수(각도->방향, 땅체크, 벽체크, Flip, 다른 오브젝트와의 거리)
+    Vector3 AngleToDir(float angle)
     {
         float radian = angle * Mathf.Deg2Rad;
         return new Vector3(Mathf.Cos(radian), Mathf.Sin(radian), 0);
@@ -188,49 +234,9 @@ public class Monster : MonoBehaviour
         checkObstacleDistance *= -1;
         facingDir *= -1;
     }
-
-
-    #region 피격
-    private void OnTriggerEnter(Collider other)
+    public float getDistanceOther(GameObject other)
     {
-        if (!animator.GetBool("isLive"))
-            return;
-
-        if (other == null)
-            return;
-
-        if (other.gameObject.tag == "PlayerAttack")
-            StartCoroutine(OnDamage());
-        
-    }
-    IEnumerator OnDamage()
-    {
-        animator.SetTrigger("takeAttack");
-        curHp -= player.GetComponent<PlayerController>().damage;
-
-        collider.enabled = false;
-        yield return new WaitForSeconds(1f);
-        collider.enabled = true;
-        Debug.Log("공격 받았음");
-    }
-    #endregion
-
-    #region 지나치기
-    private void OnTriggerStay(Collider other)
-    {
-        if (!animator.GetBool("isLive"))
-            return;
-        if (other == null)
-            return;
-        if (other.gameObject.tag == "Player")
-        {
-            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
-            rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
-
-            Vector3 dir = new Vector3(other.gameObject.transform.position.x - transform.position.x, 0f, 0f).normalized;
-            rb.AddForce(dir * 15f, ForceMode.Impulse);
-            Debug.Log("밀어내기");
-        }
+        return (other.transform.position - transform.position).magnitude;
     }
     #endregion
 }
