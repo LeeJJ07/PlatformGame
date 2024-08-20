64eca622f832d04841efcb5949b6c965cabe3eab
orpul1 <79825952+orpul1@users.noreply.github.com>
2024-08-20 13:59:13 +0900

    feat: 위험구역 표시기능 추가
    이슈: 위험구역 오브젝트 머테리얼의 알파값 변경 안됨

---
+++ Royal Rescue/Assets/Boss/Scripts/DangerZone/DangerZoneBehavior.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DangerZoneBehavior : MonoBehaviour
+{
+    [SerializeField]Material dangerZoneMaterial;
+    [SerializeField, Range(0,2f)] float alphaChangeSpeed = 1;
+    int sign = 1;
+    Color dangerzoneNewColor = new Color();
+    [SerializeField, Range(0, 1f)]
+    float min = 0.5f, max = 0.9f;
+
+    //TODO: Destory로 하는게 맞겠죠?
+    private void Start()
+    {
+        dangerzoneNewColor = dangerZoneMaterial.color;
+        Destroy(gameObject, 2f);
+    }
+    void Update()
+    {
+        //Debug.Log("color.a: " + dangerzoneNewColor.a+"\nSign: "+sign);
+
+        if(dangerzoneNewColor.a >= max)
+        {
+            sign *= -1;
+        }
+        else if(dangerzoneNewColor.a <= min)
+        {
+            sign *= -1;
+        }
+        dangerzoneNewColor.a -= alphaChangeSpeed * sign * Time.deltaTime;
+        dangerZoneMaterial.color = dangerzoneNewColor;
+    }
+}
