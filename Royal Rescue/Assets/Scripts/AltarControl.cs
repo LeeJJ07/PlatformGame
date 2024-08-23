aed9b661288bc8e214ca583c1802977520f55c69
skytower144 <127186795+catnip144@users.noreply.github.com>
2024-08-23 19:06:23 +0900

    feat: 스테이지 문 열리는 애니메이션, 카메라 전환 추가
    스테이지 문(포탈)이 열릴 때 잠깐 카메라가 포탈에 포커싱하도록 만듦

--- Royal Rescue/Assets/Scripts/AltarControl.cs
+++ Royal Rescue/Assets/Scripts/AltarControl.cs
@@ -6,8 +6,19 @@ public class AltarControl : MonoBehaviour
 {
     public static int redGem, whiteGem, greenGem = 0;
 
+    [SerializeField] private Transform stagePortal;
+    [SerializeField] private Animator coffinAnim;
+
+    private CameraFollow cameraFollow;
     private int activatedAltars = 0;
     private const int TOTALALTARS = 3;
+
+    [SerializeField] private bool test; /////
+
+    void Start()
+    {
+        cameraFollow = GameObject.FindWithTag("MainCamera").GetComponent<CameraFollow>();
+    }
 
     public bool CanActivateAltar(GemType gemType)
     {
@@ -26,6 +37,7 @@ public class AltarControl : MonoBehaviour
                 return false;
         }
     }
+
     public void ActivateAltar()
     {
         ++activatedAltars;
@@ -35,7 +47,20 @@ public class AltarControl : MonoBehaviour
     private void TryOpenStagePortal()
     {
         if (activatedAltars == TOTALALTARS)
+        {
             Debug.Log("다음 스테이지로의 포탈이 열렸습니다.");
+            StartCoroutine(PlayPortalAnimation());
+        }
+    }
+
+    IEnumerator PlayPortalAnimation()
+    {
+        cameraFollow.SetFollowTarget(stagePortal);
+        coffinAnim.Play(AnimationHash.COFFINDOOR_OPEN);
+
+        yield return new WaitForSeconds(2.5f);
+
+        cameraFollow.SetFollowTarget();
     }
 }
 
