fd4fd17726bbcc6175c571e8fa24dabd90095b69
LeeJJ07 <jkj9513@naver.com>
2024-08-26 08:15:04 +0900

    feat : MiniBoss 기본 세팅 완료

---
+++ Royal Rescue/Assets/Scripts/MiniBoss/CheckMiniBossHp.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckMiniBossHp : INode
+{
+    public void AddNode(INode node)
+    {
+    }
+
+    public INode.NodeState Evaluate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
