c974fa5b65f7c21b0c0adb00d58b305ac6df183f
skytower144 <127186795+catnip144@users.noreply.github.com>
2024-08-27 01:42:36 +0900

    feat: 무너지는 플랫폼, 캐논, 방 색깔 변경
    * 무너지는 플랫폼 구현 완료
    - 무너지는 플랫폼 프리팹 생성
    
    * 캐논볼 쏘는 캐논 구현
    - 캐논, 캐논볼 프리팹 생성
    
    * 방 색깔 붉은색 계열로 변경
    - 붉은색 벽돌 텍스처 임포트

---
+++ Royal Rescue/Assets/Scripts/LevelDesign/Cannon.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    [SerializeField] private float fireInterval;
+    [SerializeField] private GameObject cannonBall;
+    [SerializeField] private ParticleSystem explosionEffect;
+    [SerializeField] private Transform cannon, spawnPoint;
+
+    void Start()
+    {
+        InvokeRepeating("FireCannonBall", 0.2f, fireInterval);
+    }
+
+    void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    void FireCannonBall()
+    {
+        Instantiate(cannonBall, spawnPoint.position, cannon.rotation);
+        explosionEffect.Play();
+    }
+}
