b5514f3974e72ce03869c496a656b31be7708d07
Jang-wj <jwj5230@gmail.com>
2024-09-10 21:52:10 +0900

    feat : MiniBossCamera 추가

---
+++ Royal Rescue/Assets/Scripts/LevelDesign/MiniBossCam.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniBossCam : DoorTrap
+{
+    private MiniBossAI boss;
+    [SerializeField] protected Camera bossCamera;
+    protected override void Start()
+    {
+        boss = monsterHub.GetComponentInChildren<MiniBossAI>(true);
+        trapTrigger = GetComponent<BoxCollider>();
+    }
+
+    protected override IEnumerator TrapPlayer()
+    {
+        GameDirector.instance.PlayerControl.FixatePlayerRigidBody(true);
+
+        portal.gameObject.SetActive(false);
+        SwitchCamera(mainCamera, doorCamera);
+        CloseIronWall();
+        yield return new WaitForSeconds(1f);
+
+        SwitchCamera(mainCamera, doorCamera);
+
+        GameDirector.instance.PlayerControl.FixatePlayerRigidBody(false);
+        StartCoroutine(BossPlayer());
+    }
+
+    protected IEnumerator BossPlayer()
+    {
+        SwitchCamera(mainCamera, bossCamera);
+
+        yield return new WaitForSeconds(1f);
+        SwitchCamera(bossCamera, mainCamera);
+    }    
+
+    protected override bool CheckRoomClear()
+    {
+        return !(boss.gameObject.activeSelf);
+    }
+}
