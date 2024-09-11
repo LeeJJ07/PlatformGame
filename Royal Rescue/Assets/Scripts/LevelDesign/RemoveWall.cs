ff61d1d2bc536b02b74b245677de13a44d1ffe5c
Jang-wj <jwj5230@gmail.com>
2024-09-11 19:02:00 +0900

    feat : RemoveWall 파티클 추가

--- Royal Rescue/Assets/Scripts/LevelDesign/RemoveWall.cs
+++ Royal Rescue/Assets/Scripts/LevelDesign/RemoveWall.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class RemoveWall : MonoBehaviour
 {
     [SerializeField] private GameObject gem;
+    [SerializeField] private GameObject particle;
 
     // Update is called once per frame
     void Update()
@@ -12,6 +13,10 @@ public class RemoveWall : MonoBehaviour
         if (gem.activeSelf == false)
         {
             gameObject.SetActive(false);
+            GameObject effect = Instantiate(particle, transform.parent);
+            effect.transform.position = transform.position;
+            effect.GetComponent<ParticleSystem>().Play();
+            Destroy(effect, 0.9f);
         }
     }
 }
