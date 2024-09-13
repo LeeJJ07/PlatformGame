6e5dd2df46b3f5a6d6077331c1f243474f1baf1b
LeeJJ07 <jkj9513@naver.com>
2024-09-13 16:36:17 +0900

    feat : 아이템 및 인벤토리 기능 구현 중
    ** 브랜치를 빼서 작업할 예정

--- Royal Rescue/Assets/Scripts/Item/ItemDatas.cs
+++ Royal Rescue/Assets/Scripts/Item/ItemDatas.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemDatas : MonoBehaviour
+public enum ItemType
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Resource,
+    Equipable,
+    Consumable
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public enum ConsumableType
+{
+    HEAL, 
+    POWER, 
+    HEALTH, 
+    MOVESPEED
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "New Item")]
+public class ItemDatas : ScriptableObject
+{
+    [Header("Info")]
+    public string displayName;
+    public string dscription;
+    public ItemType type;
+    public Sprite icon;
+
+    [Header("Stacking")]
+    public bool canStack;
+    public int maxStackAmount;
 }
--- Royal Rescue/Assets/Scripts/PlayerScript/TestScript/TestItem.cs
+++ Royal Rescue/Assets/Scripts/PlayerScript/TestScript/TestItem.cs
@@ -2,13 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ItemType
-{
-    Equip,
-    Potion,
-    Etc
-}
-
 
 [System.Serializable]
 public class TestItem 
