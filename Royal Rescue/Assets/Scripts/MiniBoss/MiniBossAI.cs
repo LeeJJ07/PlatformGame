76f98123da235da24e53551d069a06fd19efe0c5
orpul1 <79825952+orpul1@users.noreply.github.com>
2024-09-19 13:32:12 +0900

    fix: 보스방 스프링 위치 변경 fix: 미니보스 방을 나갈 때 BossHPbar비활성화 후 다시 미니보스 방에 들어오면 활성화함

--- Royal Rescue/Assets/Scripts/MiniBoss/MiniBossAI.cs
+++ Royal Rescue/Assets/Scripts/MiniBoss/MiniBossAI.cs
@@ -67,6 +67,7 @@ public class MiniBossAI : MonoBehaviour
     bool isDie = false;
     bool isPlayDie = false;
     bool takeAttack = false;
+    bool isPlayerOutRoom = false;
     [SerializeField] private GameObject hitEffect;
     public Material material;
     private Color originalColor;
@@ -161,6 +162,8 @@ public class MiniBossAI : MonoBehaviour
         }
         if(hpbarUi)
             hpbarUi.Init((int)maxHp, null, gameObject.name);
+        if (isPlayerOutRoom)
+            hpbarUi.ActivateUI();
     }
     void Update()
     {
@@ -264,5 +267,13 @@ public class MiniBossAI : MonoBehaviour
             yield return new WaitForSeconds(0.1f);
         }
         takeAttack = false;
+    }
+    private void OnDisable()
+    {
+        if(hpbarUi)
+        {
+            hpbarUi.DeActivateUI();
+            isPlayerOutRoom = true;
+        }
     }
 }
